2c0c36d24219032efb48208ba4f1ec6136f6f3ad
Qasim <qasimcanadian@gmail.com>
2013-06-01 15:40:07 -0400

    Created AssignmentTab

---
+++ AssignmentTab.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/** The 'Assignments' tab of the MainView. Provides an interface for the user
+ * to change the Course's assignments, as well as list them.
+ */
+public class AssignmentTab {
+  public static void main(String[] args) {
+  
+  }
+
+  public AssignmentTab() {
+    // Constructor
+  }
+} // End AssignmentTab
