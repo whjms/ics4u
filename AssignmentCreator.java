e8c37330ea0cd884279f56aaac3fbe86eb7eeb2c
Qasim <qasimcanadian@gmail.com>
2013-06-02 18:53:00 -0400

    Completed initUI(), created layoutUI()

--- AssignmentCreator.java
+++ AssignmentCreator.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 /** A window that lets the user specify a new assignment's properties,
  * including its name, totals for each category, and weightings
@@ -9,13 +10,12 @@ public class AssignmentCreator extends JDialog {
   private JLabel nameLabel; // Instructs user to enter assignment name
   private JTextField nameField; // Field for assignment name entry
 
-  private JLabel[4] categoryLabels; // Labels that specify mark category names
-
-  private JPanel totalPanel;  // Panel that contains fields for entry of totals
-  private JTextField[4] totalFields;  // Text fields for category totals
-
-  private JPanel weightingPanel;  // Panel that contains fields for weightings
-  private JTextField[4] weightingFields;  // Text fields for category weightings
+  private JPanel mainPanel;  // Contains labels and entry fields
+  private JLabel[] categoryLabels; // Labels that specify mark category names
+  private JTextField[] totalFields;  // Text fields for category totals
+  private JTextField[] weightingFields;  // Text fields for category weightings
+  private JLabel totalLabel;  // Heading for total fields
+  private JLabel weightingLabel;  // Heading for weighting fields
 
   private JPanel buttonPanel;  // Contains buttons
   private JButton okButton;
@@ -35,14 +35,51 @@ public class AssignmentCreator extends JDialog {
     super(mainWindow, "Create Assignment", true);  // Creates a modal dialog 
                                                    // that disables mainWindow
     this.initUI();
+    this.layoutUI();
     this.registerControllers();
   }
 
-  /** Initializes UI components and sets up layout
+  /** Initializes UI components
    */
   private void initUI() {
     this.contentPane = new JPanel();
     this.setContentPane(this.contentPane);
+    this.contentPane.setLayout(new BoxLayout(this.contentPane,
+            BoxLayout.Y_AXIS));
+
+    this.mainPanel = new JPanel();
+    this.mainPanel.setLayout(new GridLayout(3, 4));
+
+    // Initialize labels
+    this.categoryLabels = new JLabel[4];
+    this.categoryLabels[Course.COMMUNICATION] = new JLabel("C/O:");
+    this.categoryLabels[Course.APPLICATION] = new JLabel("AP:");
+    this.categoryLabels[Course.KNOWLEDGE] = new JLabel("K/U:");
+    this.categoryLabels[Course.THINKING] = new JLabel("T/I:");
+
+    // Initialize text fields 
+    for(int i = 0; i < 3; i++) {
+      this.totalFields[i] = new JTextField();
+      this.weightingFields[i] = new JTextField();
+    }
+
+    // Initialize buttons 
+    this.okButton = new JButton("OK");
+    this.cancelButton = new JButton("Cancel");
+
+    // Initialize labels 
+    this.totalLabel = new JLabel("Totals");
+    this.weightingLabel = new JLabel("Weightings");
+
+    this.buttonPanel = new JPanel();
+  }
+
+  /** Manage laying out components for this window 
+   */
+  private void layoutUI() {
+    this.contentPane.setLayout(new BoxLayout(this.contentPane,
+            BoxLayout.Y_AXIS));
+    this.mainPanel.setLayout(new GridLayout(3, 4));
   }
 
   /** Registers controllers for buttons 
