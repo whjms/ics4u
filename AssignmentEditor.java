4351b252ddd720b22620d9f0ff9626efb6c0010e
Qasim Ali <qasimcanadian@gmail.com>
2013-06-04 20:06:07 -0400

    Fixed typo in layoutUI()

--- AssignmentEditor.java
+++ AssignmentEditor.java
@@ -15,6 +15,8 @@ public class AssignmentEditor extends JDialog {
   private JTextField[] weightings;  // Fields for user to enter weightings 
   private JTextField[] totals;   // Fields for user to enter category totals 
 
+  private JLabel totalLabel;    // Heading for totals section 
+  private JLabel weightingLabel;  // Heading for weightings section
   private JPanel buttonPanel;   // Container for buttons 
   private JButton okButton;
   private JButton cancelButton;
@@ -39,5 +41,33 @@ public class AssignmentEditor extends JDialog {
   private void initUI() {
     this.contentPane = new JPanel();
     this.setContentPane(this.contentPane);
+
+    this.mainPanel = new JPanel();
+
+    // Initialize text fields 
+    for(int i = 0; i < 3; i++) {
+      this.weightings[i] = new JTextField();
+      this.totals[i] = new JTextField();
+    }
+
+    // Initialize labels 
+    this.categories[Course.C] = new JLabel("C/O:");
+    this.categories[Course.A] = new JLabel("AP:");
+    this.categories[Course.K] = new JLabel("K/U:");
+    this.categories[Course.T] = new JLabel("T/I:");
+    this.totalLabel = new JLabel("Totals");
+    this.weightingLabel = new JLabel("Weightings");
+
+    // Initialize buttons 
+    this.okButton = new JButton("OK");
+    this.cancelButton = new JButton("Cancel");
+  }
+
+  /** Sets up the layout for UI components 
+   */
+  private void layoutUI() {
+   this.contentPane.setLayout(new BoxLayout(this.contentPane,
+          BoxLayout.Y_AXIS));
+   this.mainPanel.setLayout(new GridLayout(3, 5));
   }
 } 
