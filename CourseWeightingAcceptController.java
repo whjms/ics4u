82c24cdfe4c39082f42168ae719a92a37a1bdaac
Qasim Ali <qasimcanadian@gmail.com>
2013-06-13 17:47:05 -0400

    Created CourseWeightingAcceptController

---
+++ CourseWeightingAcceptController.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+/** Event handler for the 'OK' button on CourseWeightingEditor. Validates
+ * user input, then passes the new weightings to the course.
+ */
+
+public class CourseWeightingAcceptController implements ActionListener {
+	
+  /** Create this controller.
+   * @param course    the course to send the new values to
+   * @param fields    the fields to read weightings from
+   */
+	public CourseWeightingAcceptController(Course course, JTextField[] fields) {
+  }
+  
+  public void ActionPerformed(ActionEvent e) {
+  }
+}
+
