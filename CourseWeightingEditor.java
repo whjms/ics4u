c89e5b10dae01da65e431cc84fe2dd27ac494b98
Qasim Ali <qasimcanadian@gmail.com>
2013-06-13 17:22:46 -0400

    Created CourseWeightingEditor

---
+++ CourseWeightingEditor.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.border.*;
+
+/** A dialog that allows the user to edit a course's weightings for each
+ * category
+ * @author Qasim Ali
+ */
+public class CourseWeightingEditor extends JDialog {
+  private Course course;
+
+  /** Creates an editor for the specified course 
+   * @param course    the course to edit 
+   */
+  public CourseWeightingEditor(Course course) {
+  }
+} 
