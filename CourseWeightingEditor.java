cea446bf7c26d042e2285864c555c8d281a64fb4
Qasim Ali <qasimcanadian@gmail.com>
2013-06-13 17:43:01 -0400

    Completed initUI, layoutUI, and registerControllers

--- CourseWeightingEditor.java
+++ CourseWeightingEditor.java
@@ -8,9 +8,11 @@ import java.awt.border.*;
  */
 public class CourseWeightingEditor extends JDialog {
   private Course course;
+  private JPanel mainPanel;
   private JPanel contentPane;
   private JLabel[] categoryLabels;  // Contains labels for category fields
   private JTextField[] categoryFields;  // Contains category fields
+  private JButton okButton;
 
   /** Creates an editor for the specified course 
    * @param course    the course to edit 
@@ -20,6 +22,7 @@ public class CourseWeightingEditor extends JDialog {
     this.course = course;
     
     this.initUI();
+    this.layoutUI();
     this.registerControllers();
     this.pack();
     this.setVisible(true);
@@ -28,6 +31,7 @@ public class CourseWeightingEditor extends JDialog {
   /** Initializes UI components
    */
   private void initUI() {
+    this.mainPanel = new JPanel();
     this.contentPane = new JPanel();
     this.setContentPane(this.contentPane);
     
@@ -40,9 +44,48 @@ public class CourseWeightingEditor extends JDialog {
     this.categoryLabels[Course.A] = new JLabel("AP:");
     this.categoryLabels[Course.K] = new JLabel("K/U:");
     
-    // Initialize text fields
-    for(int i = 0; i < 3; i++) {
-      this.categoryFields = new JTextField();
-    }
+    // Initialize fields with values from the course
+    this.categoryFields[Course.C] = new JTextField(Integer.toString(
+            this.course.getCourseWeightings(Course.C)));
+    this.categoryFields[Course.T] = new JTextField(Integer.toString(
+            this.course.getCourseWeightings(Course.T)));
+    this.categoryFields[Course.A] = new JTextField(Integer.toString(
+            this.course.getCourseWeightings(Course.A)));
+    this.categoryFields[Course.K] = new JTextField(Integer.toString(
+            this.course.getCourseWeightings(Course.K)));
+            
+    this.okButton = new JButton("OK");
+  }
+  
+  private void layoutUI() {
+    this.mainPanel.setLayout(new GridLayout(4, 2));  // content pane is 4x2
+    
+    // Add elements row by row - row 1
+    this.mainPanel.add(this.categoryLabels[Course.C]);
+    this.mainPanel.add(this.categoryFields[Course.C]);
+    // Row 2
+    this.mainPanel.add(this.categoryLabels[Course.T]);
+    this.mainPanel.add(this.categoryFields[Course.T]);
+    // Row 3
+    this.mainPanel.add(this.categoryLabels[Course.A]);
+    this.mainPanel.add(this.categoryFields[Course.A]);
+    // Row 4
+    this.mainPanel.add(this.categoryLabels[Course.K]);
+    this.mainPanel.add(this.categoryFields[Course.K]);
+    
+    this.contentPane.setLayout(new BoxLayout(this.contentPane, 
+            BoxLayout.Y_AXIS));
+    
+    this.contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));  //10px padding
+    this.contentPane.add(this.mainPanel);
+    this.contentPane.add(this.okButton);
+  }
+  
+  private void registerControllers() {
+    CourseWeightingAcceptController okController = 
+            new CourseWeightingAcceptController(this.course,
+                    this.categoryFields);
+    
+    this.okButton.addActionListener(okController);
   }
 } 
