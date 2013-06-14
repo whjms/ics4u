f946c5f11b511bb3f12c099d48c801cef396f8c0
Qasim Ali <qasimcanadian@gmail.com>
2013-06-14 16:38:07 -0400

    Created FileManager

---
+++ FileManager.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+/** Loads and saves Courses
+ * @author Qasim Ali
+ */
+public class FileManager {
+  public static void saveCourse(String fileName) {
+  }
+  
+  public static Course loadCourse(String fileName) {
+  }
+}
