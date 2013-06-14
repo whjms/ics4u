2cf6b1df0b2017fb1a3691de7c946461108d64f8
Qasim Ali <qasimcanadian@gmail.com>
2013-06-14 14:45:49 -0400

    Finished layoutUI in StartupWindow

--- StartupWindow.java
+++ StartupWindow.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
-import java.awt.border.*;
+import java.awt.*;
+import javax.swing.border.*;
 
 /** Window that appears before the main window. Allows the user to either create
  * a new course or load an existing one.
@@ -8,8 +9,9 @@ import java.awt.border.*;
  public class StartupWindow extends JFrame {
    private JPanel newCoursePanel; // Contains components for creating new course
    private JPanel loadCoursePanel; // Contains components for loading course
-   private JPanel contentPane;  // Main panel
+   private JPanel contentPane;
    private JLabel welcomeLabel;  // Provides instructions for the user
+   private JLabel codeLabel;  // Instructs the user to enter course code
    
    // Radio buttons used to select between new and loading courses
    private JRadioButton loadButton;
@@ -22,11 +24,17 @@ import java.awt.border.*;
    private JButton okButton;  // Allows the user to confirm and close the window
    private JButton fileButton;  // Allows the user to select a file
    
+   private JPanel mainPanel;  // Contains all components except for ok button
+   
    public StartupWindow() {
      super("GRADEschool 2K13");
      this.initUI();
      this.layoutUI();
      this.registerControllers();
+     this.pack();
+     // This window will only be closed when the user clicks the 'ok' button
+     this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+     this.setVisible(true);
    }
    
    /** Initialize UI components
@@ -35,23 +43,59 @@ import java.awt.border.*;
       // Initialize panels
       this.contentPane = new JPanel();
       this.setContentPane(this.contentPane);
+      this.mainPanel = new JPanel();
       this.newCoursePanel = new JPanel();
       this.loadCoursePanel = new JPanel();
       
+      // Initialize buttons
       this.loadButton = new JRadioButton("Load course");
       this.newButton = new JRadioButton("New course");
       this.group = new ButtonGroup();
       this.group.add(loadButton);
       this.group.add(newButton);
+      this.fileButton = new JButton("Select file...");
+      this.okButton = new JButton("OK");
       
-      this.fileName = new JTextField();
+      // Initialize text fields
+      this.fileName = new JTextField("       ");
       this.fileName.setEditable(false);  // User can only change filename by
                                          // opening file chooser dialog
+      this.courseCode = new JTextField("      ");
+      
+      // Initialize labels
+      this.codeLabel = new JLabel("Course code:");
+      this.welcomeLabel = new JLabel("<html>Welcome to GRADEschool 2K13. Please "
+              + "load an existing course or create a new one.</html>");
     }
     
     /** Layout UI components
      */
     private void layoutUI() {
+      GridLayout mainLayout = new GridLayout(4, 2);
+      mainLayout.setHgap(5); // Add horizontal space between components
+      
+      this.mainPanel.setLayout(mainLayout);
+      this.contentPane.setLayout(new BoxLayout(this.contentPane, 
+              BoxLayout.Y_AXIS));
+              
+      this.mainPanel.add(this.loadButton);
+      this.mainPanel.add(new JPanel()); // Empty panel used for spacing
+      this.mainPanel.add(this.fileButton);
+      this.mainPanel.add(this.fileName);
+      this.mainPanel.add(this.newButton);
+      this.mainPanel.add(new JPanel()); // Empty panel
+      this.mainPanel.add(this.codeLabel);
+      this.mainPanel.add(this.courseCode);
+      
+      // Ensure the components are in the center of contentPane
+      this.okButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+      this.welcomeLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+      
+      this.contentPane.setBorder(new EmptyBorder(10,10,10,10)); // 10px padding
+      this.mainPanel.setBorder(new EmptyBorder(0,0,10,0)); // Add 10px gap on bottom
+      this.contentPane.add(this.welcomeLabel);
+      this.contentPane.add(this.mainPanel);
+      this.contentPane.add(this.okButton);
     }
     
     /** Register controllers for radio buttons, ok button, and file chooser 
