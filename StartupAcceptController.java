09cfd16653298328ae1ca6a64b4b94ee4c17ae69
Qasim Ali <qasimcanadian@gmail.com>
2013-06-14 16:00:41 -0400

    Created StartupAcceptController

---
+++ StartupAcceptController.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+/** Either creates a new course based on the user's chosen course code or gets
+ * one from FileManager based on the chosen course file's name when the user
+ * clicks the 'ok' button in StartupWindow
+ */
+public class StartupAcceptController implements ActionListener {
+}
