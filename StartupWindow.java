116bb2d850916c91f20aad3f85775e862fa58e9b
Qasim Ali <qasimcanadian@gmail.com>
2013-06-14 14:16:19 -0400

    Finished component declarations

--- StartupWindow.java
+++ StartupWindow.java
@@ -6,6 +6,22 @@ import java.awt.border.*;
  * @author Qasim Ali
  */
  public class StartupWindow extends JFrame {
+   private JPanel newCoursePanel; // Contains components for creating new course
+   private JPanel loadCoursePanel; // Contains components for loading course
+   private JPanel contentPane;  // Main panel
+   private JLabel welcomeLabel;  // Provides instructions for the user
+   
+   // Radio buttons used to select between new and loading courses
+   private JRadioButton loadButton;
+   private JRadioButton newButton;
+   private ButtonGroup group;  // Used to prevent multiple buttons from being
+                               // selected
+   private JTextField fileName;  // Displays the chosen course's filename
+   private JTextField courseCode;  // Allows the user to enter a new course's 
+                                   // code
+   private JButton okButton;  // Allows the user to confirm and close the window
+   private JButton fileButton;  // Allows the user to select a file
+   
    public StartupWindow() {
      super("GRADEschool 2K13");
      this.initUI();
@@ -16,6 +32,21 @@ import java.awt.border.*;
    /** Initialize UI components
     */
     private void initUI() {
+      // Initialize panels
+      this.contentPane = new JPanel();
+      this.setContentPane(this.contentPane);
+      this.newCoursePanel = new JPanel();
+      this.loadCoursePanel = new JPanel();
+      
+      this.loadButton = new JRadioButton("Load course");
+      this.newButton = new JRadioButton("New course");
+      this.group = new ButtonGroup();
+      this.group.add(loadButton);
+      this.group.add(newButton);
+      
+      this.fileName = new JTextField();
+      this.fileName.setEditable(false);  // User can only change filename by
+                                         // opening file chooser dialog
     }
     
     /** Layout UI components
@@ -36,6 +67,6 @@ import java.awt.border.*;
     
     /** Disable all components within the newCoursePanel
      */
-    publid void disableNew() {
+    public void disableNew() {
     }
  }
