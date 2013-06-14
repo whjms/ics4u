56d269b7d8dd4fcab535df3f6419aa96726818b5
Qasim Ali <qasimcanadian@gmail.com>
2013-06-14 16:10:39 -0400

    Renamed StartupAcceptController to StartupAcceptHandler

--- StartupAcceptController.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-import java.awt.event.*;
-
-/** Either creates a new course based on the user's chosen course code or gets
- * one from FileManager based on the chosen course file's name when the user
- * clicks the 'ok' button in StartupWindow
- */
-public class StartupAcceptController implements ActionListener {
-}
