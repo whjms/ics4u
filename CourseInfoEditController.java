6c79692f2af3f24a11dec5ab70322dd02a52c0f1
Qasim Ali <qasimcanadian@gmail.com>
2013-06-15 12:28:51 -0400

    Completed CourseInfoEditController

--- CourseInfoEditController.java
+++ CourseInfoEditController.java
@@ -5,4 +5,23 @@ import java.awt.event.*;
  * @author Qasim Ali
  */
 public class CourseInfoEditController implements ActionListener {
+  private Course course;  // Main model
+  private MainWindow mainWindow;  // Window to disable
+  
+  /** Create this handler for the given Course
+   * @param course    the Course to edit
+   * @param mw        the MainWindow to disable 
+   */
+  public CourseInfoEditController(Course course, MainWindow mw) {
+    this.course = course;
+    this.mainWindow = mw;
+  }
+  
+  /** Open a CourseWeightingEditor
+   * @param e    the triggered event
+   */
+  public void ActionPerformed(ActionEvent e) {
+    CourseWeightingEditor editor = new CourseWeightingEditor(this.course,
+            this.mw);
+  }
 }
