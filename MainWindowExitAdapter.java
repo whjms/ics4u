d089eeeb3e4d02790c1df386cc24f43e49787f1d
Qasim Ali <qasimcanadian@gmail.com>
2013-06-15 14:52:07 -0400

    Created MainWindowExitAdapter

---
+++ MainWindowExitAdapter.java
@@ -0,0 +1,7 @@
+import java.awt.event.*;
+
+/** Event listener that automatically saves the Course when a MainWindow closes
+ * @author Qasim Ali
+ */
+public class MainWindowExitAdapter extends WindowAdapter {
+}
