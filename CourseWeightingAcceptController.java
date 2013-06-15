0b09be8bc780bc3f0cddb87a42622e78aca018ea
Qasim Ali <qasimcanadian@gmail.com>
2013-06-15 12:52:01 -0400

    Added exception handling for total sum != 100, new totals are now sent to Course

--- CourseWeightingAcceptController.java
+++ CourseWeightingAcceptController.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.IOException;
 
 /** Event handler for the 'OK' button on CourseWeightingEditor. Validates
  * user input, then passes the new weightings to the course.
@@ -27,14 +28,28 @@ public class CourseWeightingAcceptController implements ActionListener {
     try {
       // Create a list of weighting values
       int[] weights = new int[4];
+      int sum = 0;  // used to make sure totals are valid
       
       for(int i = 0; i < 4; i++) {
         weights[i] = Integer.parseInt(fields[i].getText());
+        sum += weights[i]
         
         // Make sure weightings are >= 0
         if(weights[i] < 0)
           throw new NumberFieldException(weights[i], "course weightings");
       }
+      
+      // Totals don't add up to 100, so something's off
+      if(sum != 100)
+        throw new IOException();  // This should be a custom exception, but use
+                                  // this for testing
+      
+      // Send the values to the course
+      this.course.setCourseWeightings(weights);
+      
+      // Close the editor
+      this.editor.dispose();
+      
     }
     catch(NumberFormatException ex) {
       // Alert the user if a value cannot be parsed
@@ -45,6 +60,12 @@ public class CourseWeightingAcceptController implements ActionListener {
     catch(NumberFieldException ex) {
       JOptionPane.showMessageDialog(this.editor, ex.toString(),
               "Error editing course", JOptionPane.ERROR_MESSAGE);
+    }
+    // ex's type should be changed to a custom exception later on: sum != 100
+    catch(IOException ex) {
+      JOptionPane.showMessageDialog(this.editor, "Error editing course: "
+              + "weightings must add up to 100.", "Error editing course",
+              JOptionPane.ERROR_MESSAGE);
     }
   }
 }
